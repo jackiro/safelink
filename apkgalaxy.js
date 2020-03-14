b4062a2e220a3f5264764f125d4d6a0befa2935b
Achmad Ja'far Shodiq <ach.jafar.sh@gmail.com>
2020-03-14 22:53:11 +0700

    Safelink ApkGalaxy

--- apkgalaxy.js
+++ apkgalaxy.js
@@ -18,12 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*3)+ 1);
-var xxx=null;
-if(x=="1"){xxx="what-benefits-do-you-get-in-digital.html"}
-if(x=="2"){xxx="do-you-need-big-budget-for-digital.html"}
-if(x=="3"){xxx="what-does-digital-marketer-do-digital.html"}a_to_ve[i].href="https://business.androvie.xyz/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://www.apkgalaxy.xyz/2020/03/whatsapp-mod-apk-anti-banned-full-fitur.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
