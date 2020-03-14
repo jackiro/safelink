dbb85ae92c69a704b743bd5023449ed7ff4b22d9
Achmad Ja'far Shodiq <ach.jafar.sh@gmail.com>
2020-03-14 22:53:47 +0700

    Safelink ApkGalaxy.xyz

--- apkgalaxy.js
+++ apkgalaxy.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://www.apkgalaxy.xyz/2020/03/whatsapp-mod-apk-anti-banned-full-fitur.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://dl-galaxy.blogspot.com/2020/03/download-aplikasi-android.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
